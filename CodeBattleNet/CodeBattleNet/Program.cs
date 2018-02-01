68726404e930eb1aa214e4cf7fedf9a48f604872
Igor <Ins137@gmail.com>
2018-02-02 02:58:09 +0400

    c# client stub

---
+++ CodeBattleNet/CodeBattleNet/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using CodeBattleNetLibrary;
+
+namespace CodeBattleNet
+{
+	internal static class Program
+	{
+		public static void Main()
+		{
+			var r = new Random();
+			var gcb = new GameClientKata("epruizhw0172.moscow.epam.com:8080", "csharp@mail.org", "1147975910246182505");
+            gcb.Run(() =>
+            {
+                if (gcb.Questions.Count == 0)
+                {
+                    gcb.
+                }
+
+                var answers = new List<string>();
+                foreach (var q in gcb.Questions)
+                {
+                    answers.Add(solve(gcb.Level, q));
+                }
+                gcb.
+			});
+
+			Console.ReadKey();
+		}
+
+        private static string solve(int level, string question)
+        {
+            switch(level)
+            {
+                case 0:
+                    {
+                        if (question.Equals("hello"))
+                        {
+                            return "world";
+                        }
+                        else if (question.Equals("world"))
+                        {
+                            return "hello";
+                        }
+                        return question;                
+                    }
+                default:
+                    return "TODO: solve quiz";
+                    
+            }
+        }
+	}
+}
---
+++ CodeBattleNet/CodeBattleNetLibrary/GameClientBomberman.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using WebSocket4Net;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace CodeBattleNetLibrary
+{
+	public class GameClientKata
+	{
+		private readonly WebSocket _socket;
+		private event Action OnUpdate;
+        public int Level { get; private set; }
+        public IList<string> Questions { get; private set; }
+
+        public GameClientKata(string server, string user, string code)
+		{
+			_socket =
+				new WebSocket(
+					$"ws://{server}/codenjoy-contest/ws?user={user}&code={code}");
+			        _socket.MessageReceived += (s, e) => { ParseField(e.Message); };
+		}
+
+		public void Run(Action handler)
+		{
+			OnUpdate += handler;
+			_socket.Open();
+		}
+
+		public void Blank()
+		{
+			_socket.Send("");
+		}
+
+        public void sendAnswers(IList<string> answers)
+        {
+            string msg = JsonConvert.SerializeObject(answers);
+            Console.WriteLine(msg);
+        }
+
+        private void send(string answer)
+        {
+            var msg = $"message('{answer}')";
+            Console.WriteLine($"Sending: {msg}");
+            this._socket.Send(msg);
+        }
+
+		private void ParseField(string rawField)
+		{
+            Quizz quizz = JsonConvert.DeserializeObject<Quizz>(rawField.Substring(6));
+            Level = quizz.Level;
+            Questions = quizz.Questions;
+
+			OnUpdate?.Invoke();
+		}
+	}
+}
